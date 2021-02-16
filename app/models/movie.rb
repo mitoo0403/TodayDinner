cc50342b3997836a65d028d7921bb4028d1fc5b6
mitoo0403 <mitooo.19821113@gmail.com>
2021-02-16 16:14:11 +0900

    movieのバリデーション

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,20 @@
 class Movie < ApplicationRecord
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :time_required_id
+  end
+
+  belongs_to :user
+  # has_many :comments
+  has_one_attached :image
+  validates :image, presence: true
   belongs_to :category
   belongs_to :time_required
+  
 end
